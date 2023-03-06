9975d0607241a4a527361418f3fdb7bd8fa9373e
paultsviliuk <paultsviliuk@gmail.com>
2023-03-06 06:51:00 -0800

    Added smart contract test.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,12 @@
+const { task } = require("hardhat/config");
+
+require("@nomicfoundation/hardhat-toolbox");
+
+task("balance", "prints an account's balance").setAction(async () => {});
+
+
 /** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
   solidity: "0.8.18",
 };
